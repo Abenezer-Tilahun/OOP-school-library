9687268d79465388fb3c0f73264967244ab4750d
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-05-17 00:47:31 +0300

    Turn Person class to Nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -23,5 +26,9 @@ class Person
     return true if age >= 18
 
     false
+  end
+
+  def correct_name
+    @name
   end
 end
