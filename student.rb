366c32427aa60fa573fde2e5a7ef593b3780ef1b
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-05-19 18:49:28 +0300

    Update the student to check Classroom

--- student.rb
+++ student.rb
@@ -6,13 +6,13 @@ class Student < Person
 
   def initialize(age, classroom = 'Default', name = 'unknown', parent_permission: true)
     super(name, age, parent_permission)
-    @classroom = classroom
-    check = -> { classroom.students.push(self) unless classroom.students.include?(self) }
+    @classroom = Classroom.new(classroom)
+    check = -> { @classroom.students.push(self) unless @classroom.students.include?(self) }
     check.call
   end
 
   def classroom=(classroom)
-    @classroom = classroom
+    @classroom = Classroom.new(classroom)
     check.call
   end
 
