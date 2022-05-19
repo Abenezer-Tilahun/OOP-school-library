e8428a6390ac830b7ca0f3831baa153e9060cb62
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-05-19 18:38:03 +0300

    Add class variable to store books

--- book.rb
+++ book.rb
@@ -4,10 +4,15 @@ class Book
   attr_accessor :title, :author
   attr_reader :rentals
 
+  # rubocop:disable Style/ClassVars
+  @@books = []
+  # rubocop:enable Style/ClassVars
+
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
+    @@books.push(self)
   end
 
   def add_rental(date, person)
