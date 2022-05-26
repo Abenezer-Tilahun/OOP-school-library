74b4ff1181e5c474a0693e97902d48b4b2dd9f9a
vikipretium <vighneshpasupathy@gmail.com>
2022-05-26 11:30:33 +0530

    add people data and method

--- data.rb
+++ data.rb
@@ -27,3 +27,39 @@ def write_books(books)
 
   File.write('./Data/book_file.json', JSON.generate(temp).to_s)
 end
+
+def write_people(persons)
+  temp = []
+  persons.each do |person|
+    temp << if person.instance_of? Student
+              {
+                role: 'Student',
+                name: person.name,
+                age: person.age,
+                parent_permission: person.parent_permission
+              }
+            elsif person.instance_of? Teacher
+              {
+                role: 'Teacher',
+                name: person.name,
+                age: person.age,
+                specialization: person.specialization
+              }
+            end
+  end
+
+  File.write('./data/people_file.json', JSON.generate(temp)).to_s
+end
+
+def read_people
+  people = []
+  JSON.parse(File.read('./data/people_file.json')).each do |person|
+    people << case person['role']
+              when 'Student'
+                Student.new(person['age'], person['name'], parent_permission: person['parent_permission'])
+              when 'Teacher'
+                Teacher.new(person['age'], person['specialization'], person['name'])
+              end
+  end
+  people
+end
