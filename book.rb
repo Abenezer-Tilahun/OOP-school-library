f29ee7c02fcae1dc9abe452f81ab028fcc801b1a
vikipretium <vighneshpasupathy@gmail.com>
2022-05-26 19:47:55 +0530

    add testing files and book_spec test

--- book.rb
+++ book.rb
@@ -16,7 +16,16 @@ class Book
   end
 
   def add_rental(date, person)
-    rental = Rental.new(date, self, person)
-    @rentals.push(rental)
+    # rental = Rental.new(date, self, person)
+    # @rentals.push(rental)
+    @rentals.push(Rental.new(date, self, person)) unless @rentals.include?(Rental.new(date, self, person))
+  end
+
+  def to_json(*_args)
+    {
+      'json_class' => self.class.name,
+      'title' => @title,
+      'author' => @author
+    }
   end
 end
