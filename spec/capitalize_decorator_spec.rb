ff33b7799ac094f17d3dcb87197e34d872d0295a
vikipretium <vighneshpasupathy@gmail.com>
2022-05-27 11:55:54 +0530

    add capitalize decorator test

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,12 @@
+require_relative 'spec_helper'
+
+describe CapitalizeDecorator do
+    context 'When testing the Basedecorator class' do
+        it 'should return \'abenezer\' when we call the correct_name method' do
+            person=Person.new(27, 'abenezer')
+            cap_decorator=CapitalizeDecorator.new(person)
+            name=cap_decorator.correct_name 
+            expect(name).to eq 'Abenezer'
+        end    
+end
+end
